4802be364c171c264d2925e16ed6a95fa25c5544
MatthewTe <teelucksingh.matthew1@gmail.com>
2020-07-09 22:51:29 -0400

    V 0.1.0 main push

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mail_data_transfer_api",
+    version="0.1.0",
+    author="MatthewTe",
+    description="An API that facilitates small data transfers through SMTP and IMAP mail servers.",
+    long_description=long_description,
+    url="https://github.com/MatthewTe/mail_socket_api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: - Alpha",
+        "Topic :: Data Science :: Pipeline API",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: Windows"
+    ],
+    install_requires=[
+        'pandas', 'imapclient']
+)
